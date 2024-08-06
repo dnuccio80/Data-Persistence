f871c6b96b51a76b5b910ccf5b17ce3eb7635b70
dnuccio80 <nuccio227@gmail.com>
2024-08-06 13:51:06 -0300

    MainMenuFinished

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
@@ -12,5 +14,31 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private Button startButton;
     [SerializeField] private Button quitButton;
 
+    private void Awake()
+    {
+        startButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+           #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+           #else
+            Application.Quit();
+           #endif
+        });
+    }
+
+    private void Start()
+    {
+        SetRandomPlayerName();
+    }
+
+    void SetRandomPlayerName()
+    {
+        nameInputField.text = "Player" + Random.Range(100, 1000);
+    }
 
 }
